61ebbd7cb4d457dd3a72984acfbebe9126a240fd
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-08-19 16:47:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <header>
+      <h1 >(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</h1> 
+    </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test.js
@@ -0,0 +1,14 @@
+function Test() {
+    return (
+      <div className="Test">
+         <header>
+        <h1 >(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</h1> 
+        <h1 >(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</h1> 
+        <h1 >(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</h1> 
+        <h1 >(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</h1> 
+      </header>
+      </div>
+    );
+  }
+  
+  export default Test;
