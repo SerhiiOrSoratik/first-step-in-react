bde8f7b884b96a0f0812feeb8863fce69b902729
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-08-20 16:31:09 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import TaskForm from './components/Task-form';
-import Todo from './components/Todo';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import TaskForm from "./components/Task-form";
+import Todo from "./components/Todo";
 
-let idCount = 1
+let idCount = 1;
 
 function App() {
-
-const createTask = (data) => {
-  return {
-    id: idCount++,
-    title: data.title,
-    done: false,
-    due_date: data.due_date || "",
-    description: data.description || "",
+  const createTask = (data) => {
+    return {
+      id: idCount++,
+      title: data.title,
+      done: false,
+      due_date: data.due_date || "",
+      description: data.description || "",
+    };
   };
-};
 
-const [taskList, addTask] = useState([]);
+  const [taskList, addTask] = useState([]);
 
-const addNewTask = (task) => {
-  const newTask = createTask(task);
-  addTask([...taskList, newTask])
-}
+  const addNewTask = (task) => {
+    const newTask = createTask(task);
+    addTask([...taskList, newTask]);
+  };
 
   return (
     <div className="main">
       <div className="todo">
-      <Header />
-      <TaskForm onSubmit={addNewTask}/>
-      <Todo tasks={taskList}/>
+        <Header />
+        <TaskForm onSubmit={addNewTask} />
+        <Todo tasks={taskList} />
       </div>
     </div>
-   
   );
 }
-
 
 export default App;
