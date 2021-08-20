75049f0eba1def6ae4208f1979b97f32e2fbcb6a
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-08-20 15:23:23 +0300

    add task form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import TaskForm from './components/Task-form';
 import Todo from './components/Todo';
 
-const inc =
-  (init = 0) =>
-  () =>
-    ++init;
-const genId = inc();
+let idCount = 1
+
+function App() {
 
 const createTask = (data) => {
   return {
-    id: genId(),
+    id: idCount++,
     title: data.title,
     done: false,
     due_date: data.due_date || "",
@@ -18,52 +18,24 @@ const createTask = (data) => {
   };
 };
 
-// const options = { year: "numeric", month: "numeric", day: "numeric" };
-// const date1 = new Date("8/16/2021");
-// const date2 = new Date("8/15/2021");
-// const date3 = new Date("8/17/2021");
-// const date4 = new Date("8/16/2021");
+const [taskList, addTask] = useState([]);
 
-let tasks = [
-  {
-    id: genId(),
-    title: "first task",
-    done: false,
-    due_date: "8/16/2021",
-    description: "first description",
-  },
-  {
-    id: genId(),
-    title: "second task",
-    done: true,
-    due_date: "8/16/2021",
-  },
-  {
-    id: genId(),
-    title: "third task",
-    done: false,
-    due_date: "8/16/2021",
-    description: "third description",
-  },
-  {
-    id: genId(),
-    title: "four task",
-    done: false,
-    due_date: "8/16/2021",
-  },
-];
+const addNewTask = (task) => {
+  const newTask = createTask(task);
+  addTask([...taskList, newTask])
+}
 
-function App() {
   return (
     <div className="main">
       <div className="todo">
       <Header />
-      <Todo tasks={tasks}/>
+      <TaskForm onSubmit={addNewTask}/>
+      <Todo tasks={taskList}/>
       </div>
-    
     </div>
    
   );
 }
+
 
 export default App;
