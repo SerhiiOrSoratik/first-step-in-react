fea0a8e9335a126b52370f5898b5bb629ce97943
SerhiiOrSoratik <87651890+SerhiiOrSoratik@users.noreply.github.com>
2021-08-27 11:28:19 +0300

    fix checkbox

--- src/components/Task-component/task-checkbox.jsx
+++ src/components/Task-component/task-checkbox.jsx
@@ -1,35 +1,13 @@
 const Checkbox = ({ task, changeConditionTask }) => {
   const isDone = task.done;
 
-  if (isDone) {
-    return (
-      <input
-        type="checkbox"
-        onChange={() => changeConditionTask(task.id, isDone)}
-        checked
-      />
-    );
-  } else {
-    return (
-      <input
-        type="checkbox"
-        onChange={() => changeConditionTask(task.id, isDone)}
-      />
-    );
-  }
-
-  // const isChecked = (done) => {
-  //   return done ? 'checked' : ''
-  // }
-
-  // return (
-  //   <input
-  //     type="checkbox"
-  //     onChange={() => changeConditionTask(task.id, isDone)}
-  //     ...{isDone ? 'checked' : null}
-  //   />
-  // );
-
+  return (
+    <input
+      type="checkbox"
+      onChange={() => changeConditionTask(task.id, isDone)}
+      checked={isDone}
+    />
+  );
 };
 
 export default Checkbox;
